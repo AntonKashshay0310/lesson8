3794413f6f7853d56aa5b3e072dc3d22c12b0bbf
AntonKashshay0310 <antosha.kashshay@gmail.com>
2025-02-19 20:10:52 +0200

    add

---
+++ js/script.js
@@ -0,0 +1,19 @@
+// for (let number = 0; number < 5;) {
+//     console.log(`Число ${number}`);
+// }
+// let number = 0;
+
+// while (number <= 5) {
+//     console.log(`Число ${number}`)
+//     number += 1;
+// }
+
+let num = 10;
+// while(num >= 7) {
+//     console.log(num);
+//     num -= 1;
+// }
+
+for(let i = 10; i>= 7; i -= 1) {
+console.log(i);
+}
